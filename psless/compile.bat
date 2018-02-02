207273c50ef075df3351a659d35c03ce23b13d6f
Liam Glanfield <liam@glanfield.co.uk>
2018-02-02 11:46:30 +0000

    Add files via upload

---
+++ psless/compile.bat
@@ -0,0 +1,1 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /reference:C:\Windows\Microsoft.NET\assembly\GAC_MSIL\System.Management.Automation\v4.0_3.0.0.0__31bf3856ad364e35\system.management.automation.dll /out:c:\psless\psless.exe c:\psless\psless.cs
---
+++ psless/example.ps1
@@ -0,0 +1,7 @@
+#psless.exe example.ps1
+# write-output used for output
+
+$value = $env:USERDOMAIN
+
+
+Write-Output "hello $value world"
