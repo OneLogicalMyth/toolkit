02b58a4d18225596cfcc0ed8435cbd4e27012bcd
Liam Glanfield <liam@glanfield.co.uk>
2020-06-24 13:12:17 +0100

    Update Get-NessusComplianceResults.ps1

--- reporting/nessus/Get-NessusComplianceResults.ps1
+++ reporting/nessus/Get-NessusComplianceResults.ps1
@@ -1,5 +1,5 @@
 Function Get-NessusComplianceResults {
-param($NessusFile)
+param([string[]]$NessusFile)
 
     $NessusXML = New-Object Xml
     $NessusXML.Load((Convert-Path $NessusFile))
@@ -8,14 +8,33 @@ param($NessusFile)
 
     foreach($check in $ComplianceResults)
     {
+        if($Check.'compliance-info' -match 'not identified that the chosen audit applies|This audit checks the testable')
+        {
+            continue
+        }
 
-        $Out = '' | Select-Object Hostname, Benchmark, Check, ValueObtained, ValueRequired, Result
+        $Out = '' | Select-Object Hostname, Benchmark, CheckNumber, Check, ValueObtained, ValueRequired, Result
         $Out.Hostname = $check.ParentNode.name
         $Out.Benchmark = $check.'compliance-audit-file'.Replace('_',' ') -replace '.audit'
         $Out.Check = $check.'compliance-check-name'
         $Out.ValueObtained = $check.'compliance-actual-value'
         $Out.ValueRequired = $check.'compliance-policy-value'
         $Out.Result = $check.'compliance-result'
+
+        if($Out.Benchmark -match 'CIS')
+        {
+            try
+            {
+                $Out.CheckNumber = [System.Version]($Out.Check.Split(' ')[0])
+            }
+            catch
+            {
+                $global:test = $check
+            }
+        }else{
+            $Out.CheckNumber = $null
+        }
+        
         $Out
     }
 
