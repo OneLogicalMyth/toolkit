3a4684554fc813114c3ba3ed4013856f1b6b0ee7
Liam Glanfield <liam@glanfield.co.uk>
2022-03-01 10:09:49 +0000

    Update Get-NessusPluginSummary.ps1

--- reporting/nessus/Get-NessusPluginSummary.ps1
+++ reporting/nessus/Get-NessusPluginSummary.ps1
@@ -1,4 +1,3 @@
-
 Function Get-NessusPluginSummary {
 param($NessusFile=$null,[string]$PluginName=$null,[int]$PluginID=$null,[int]$MinimumSeverity=1)
 
@@ -27,13 +26,21 @@ param($NessusFile=$null,[string]$PluginName=$null,[int]$PluginID=$null,[int]$Min
     Foreach($Item in $ReportItems)
     {
 
-        $Out = '' | Select-Object Host, PluginID, PluginName, Protocol, Port, ServiceName
+        $Out = '' | Select-Object Host, PluginID, PluginName, Protocol, Port, ServiceName, Risk, CVSSBaseScore2, CVSSVector2, CVSSBaseScore3, CVSSVector3, CVSSCVESource, Synopsis, Solution
         $Out.Host = $Item.ParentNode.name
         $Out.PluginID = $Item.pluginID
         $Out.PluginName = $Item.pluginName
         $Out.Protocol = $Item.protocol
         $Out.Port = $Item.port
         $Out.ServiceName = $Item.svc_name
+        $Out.Risk = $Item.risk_factor
+        $Out.CVSSBaseScore2 = $Item.cvss_base_score
+        $Out.CVSSVector2 = $Item.cvss_vector
+        $Out.CVSSBaseScore3 = $Item.cvss3_base_score
+        $Out.CVSSVector3 = $Item.cvss3_vector
+        $Out.Synopsis = $Item.synopsis
+        $Out.Solution = $Item.solution
+        $Out.CVSSCVESource = $Item.cvss_score_source
         $Out
 
     }
